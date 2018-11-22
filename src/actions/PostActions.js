b2828dceaef1a40853b771f4f592b5a267720045
ameer kabir <ameerkabir2@gmail.com>
2018-11-22 20:12:56 +0000

    added action for create post

--- src/actions/PostActions.js
+++ src/actions/PostActions.js
@@ -1,6 +1,7 @@
 import { FETCH_POSTS, NEW_POSTS } from "./types";
 export const fetchPost = () => dispatch => {
   console.log("fetch is hiting here");
+  debugger;
   fetch("https://jsonplaceholder.typicode.com/posts")
     .then(response => response.json())
     .then(post =>
@@ -9,4 +10,21 @@ export const fetchPost = () => dispatch => {
         payload: post
       })
     );
+};
+export const creatPost = (postData) => dispatch => {
+   console.log("hiff here")
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(postData)
+    })
+     .then(res=> res.json())
+     .then(post => dispatch({
+       type: NEW_POSTS,
+       payload: post
+
+     }))
+  
 };
